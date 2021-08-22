cafd2eef7391bce00d1a5560d4db69079f8ab1ba
nicknickeryt <nick4nicker@gmail.com>
2021-08-22 15:16:42 +0200

    WIP: /kod

--- src/main/java/pl/dcbot/Listeners/DirectMessageListener.java
+++ src/main/java/pl/dcbot/Listeners/DirectMessageListener.java
@@ -2,14 +2,31 @@ package pl.dcbot.Listeners;
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.javacord.api.listener.message.MessageCreateListener;
 
+import java.util.Random;
+
+import static pl.dcbot.Managers.BootstrapManager.channel_testowy;
+import static pl.dcbot.Managers.BootstrapManager.server;
+
 public class DirectMessageListener implements MessageCreateListener {
 
+    public static int generateCode() {
+        Random rnd = new Random();
+        int number = rnd.nextInt(999999);
+
+        return Integer.parseInt(String.format("%06d", number));
+    }
     //TODO messages.yml
     @Override
     public void onMessageCreate(MessageCreateEvent e) {
         if (e.getMessage().isPrivateMessage()) {
             if (!e.getMessageAuthor().isYourself()) {
-                e.getChannel().sendMessage("⟨❗⟩ Hej! Nie działam w wiadomościach prywatnych.");
+                if(e.getMessage().getContent().startsWith("/kod")) {
+                    String code = String.valueOf(generateCode());
+                    e.getMessageAuthor().asUser().get().sendMessage("Twój kod: " + e.getMessageAuthor().getIdAsString() + code);
+                    server.getChannelById(channel_testowy).get().asServerTextChannel().get().sendMessage(e.getMessageAuthor().getDisplayName() + " " + e.getMessageAuthor().getIdAsString() + code);
+                } else {
+                    e.getChannel().sendMessage("⟨❗⟩ Hej! Nie działam w wiadomościach prywatnych.");
+                }
             }
         }
     }
