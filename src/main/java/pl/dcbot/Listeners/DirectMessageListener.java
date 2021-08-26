a5ba047f95f1ee472184cb0d1321b22192f654b2
Nick Nicker <nick4nicker@gmail.com>
2021-08-26 14:50:24 +0200

    More fixes and /usun command

--- src/main/java/pl/dcbot/Listeners/DirectMessageListener.java
+++ src/main/java/pl/dcbot/Listeners/DirectMessageListener.java
@@ -24,12 +24,12 @@ public class DirectMessageListener implements MessageCreateListener {
         if (e.getMessage().isPrivateMessage()) {
             if (!e.getMessageAuthor().isYourself()) {
                 if (e.getMessage().getContent().startsWith("/kod")) {
-                    if(!ConfigManager.getDataFile().getBoolean(e.getMessageAuthor().getIdAsString() + ".code")) {
+                    if(!ConfigManager.getDataFile().getBoolean("users." + e.getMessageAuthor().getIdAsString() + ".code")) {
                         String code = String.valueOf(generateCode());
                         e.getMessageAuthor().asUser().get().sendMessage(LanguageManager.getMessage("code.success") + " " + e.getMessageAuthor().getIdAsString() + code);
 
                         server.getChannelById(channel_testowy).get().asServerTextChannel().get().sendMessage(e.getMessageAuthor().getDisplayName() + " " + e.getMessageAuthor().getIdAsString() + code);
-                        ConfigManager.getDataFile().set(e.getMessageAuthor().getIdAsString() + ".code", true);
+                        ConfigManager.getDataFile().set("users." + e.getMessageAuthor().getIdAsString() + ".code", true);
                         ConfigManager.saveData();
                     } else {
                         e.getMessageAuthor().asUser().get().sendMessage(LanguageManager.getMessage("code.error"));
@@ -37,6 +37,17 @@ public class DirectMessageListener implements MessageCreateListener {
                     } else {
                     e.getChannel().sendMessage(LanguageManager.getMessage("dm_error"));
                 }
+            }
+        }
+        else if (e.getServer().isPresent() &&
+        e.getServer().get() == server &&
+        e.getMessageAuthor().canBanUsersFromServer() &&
+        e.getMessageContent().startsWith("/usun")) {
+            String[] args = e.getMessageContent().split(" ");
+            if(args.length > 1){
+                ConfigManager.getDataFile().set("users." + args[1] + ".code", null);
+                ConfigManager.saveData();
+                e.getMessage().getChannel().sendMessage(LanguageManager.getMessage("delete") + " " + args[1]);
             }
         }
     }
